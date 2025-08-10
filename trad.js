4c1819aafa1e38bfc6324c92ae69714613395613
Estelink <125503633+Estelinnk@users.noreply.github.com>
2025-08-10 18:47:22 +0200

    Update trad.js

--- trad.js
+++ trad.js
@@ -1,23 +1,17 @@
-// Dictionnaire de traduction des noms techniques
 const translations = {
-    // Armes
-    "[WEAPON_PISTOL]": "Pistolet",
-    "[WEAPON_CARBINERIFLE]": "Fusil d'assaut",
-    "[WEAPON_KNIFE]": "Couteau",
-    "[WEAPON_SMG]": "Pistolet mitrailleur",
-    "[WEAPON_ASSAULTRIFLE]": "Fusil AK-47",
-
-    // Objets
-    "[water]": "Bouteille d'eau",
-    "[bread]": "Pain",
-    "[phone]": "Téléphone",
+  "[WEAPON_PISTOL]": "Pistolet",
+  "[WEAPON_CARBINERIFLE]": "Fusil d'assaut",
+  "[WEAPON_KNIFE]": "Couteau",
+  "[WEAPON_SMG]": "Pistolet mitrailleur",
+  "[WEAPON_ASSAULTRIFLE]": "Fusil AK-47",
+  "[water]": "Bouteille d'eau",
+  "[bread]": "Pain",
+  "[phone]": "Téléphone portable"
 };
-
-// Fonction utilitaire pour remplacer les noms techniques par leur traduction
-function translateText(text) {
-    if (!text) return text;
-    Object.keys(translations).forEach(key => {
-        text = text.replace(new RegExp(key, 'g'), translations[key]);
-    });
-    return text;
+function translateText(text){
+  if(!text) return text;
+  for(const k in translations){
+    text = text.replace(new RegExp(k,'g'), translations[k]);
+  }
+  return text;
 }
